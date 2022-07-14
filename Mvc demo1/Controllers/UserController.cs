737ff238983d5818490d9429d7c5ba48cfeff30d
LENOVO <LENOVO@KEERTHI>
2022-07-14 07:17:46 -0700

    Add project files.

---
+++ Mvc demo1/Controllers/UserController.cs
@@ -0,0 +1,42 @@
+﻿using Mvc_demo1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mvc_demo1.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        private DataContext _context;
+
+        public UserController()
+        {
+            _context = new DataContext();
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ToList()
+        {
+            var list = _context.users.ToList();
+            return View(list);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(User users)
+        {
+            _context.users.Add(users);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+    }
+}
---
+++ Mvc demo1/Models/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Mvc_demo1.Models
+{
+    public class DataContext:DbContext
+    {
+        public DbSet<User> users { get; set; }
+    }
+}
